39b2393e0287f5ee96e576448df074a457c64b78
snigdha15 <sngjasti@gmail.com>
2021-05-08 15:57:28 +0530

    PRACTICE_SUMS-ARRAYS

---
+++ ARRAYS_SUMS/src/one/e3.java
@@ -0,0 +1,22 @@
+package one;
+public class e3 {
+	public static void main(String[] args) {
+
+	    int[] num = {1, 2, 3, 4, 5};
+	    int toFind = 6;
+	    boolean found = false;
+
+	    for (int n : num) {
+	      if (n == toFind) {
+	        found = true;
+	        break;
+	      }
+	    }
+	    
+	    if(found)
+	      System.out.println(toFind + " is found.");
+	    else
+	      System.out.println(toFind + " is not found.");
+	  }
+	}
+
