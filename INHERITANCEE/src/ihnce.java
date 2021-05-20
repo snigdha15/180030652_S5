1d27b813aefa4ec19bca45e3382bc5cc801a66a5
snigdha15 <sngjasti@gmail.com>
2021-05-20 09:37:45 +0530

    PRACTICE_SUMS_INHERITANCE

---
+++ INHERITANCEE/src/ihnce.java
@@ -0,0 +1,6 @@
+public class ihnce{
+	public static void main(String[] args) {
+		Employee employee = new Employee("John Doe", 5000000, 2019, "321a2sd1321ad");
+		System.out.println(employee);
+	}
+}
---
+++ INHERITANCEE/src/inheri.java
@@ -0,0 +1,47 @@
+class Per {
+    String name;
+}
+
+class Student extends Per {
+    int rollno;
+}
+
+class Teacher extends Per {
+    String subject;
+    double salary;
+
+    Teacher(String n, String sub, double s) {
+        name = n;
+        subject = sub;
+        salary = s;
+    }
+
+    void display() {
+        System.out.println("Name: " + name + ", Subject: " + subject + ", Salary: " + salary);
+    }
+}
+
+class CollegeStudent extends Student {
+    int year;
+    String major;
+
+    CollegeStudent(String n, int roll, int y, String m) {
+        name = n;
+        rollno = roll;
+        year = y;
+        major = m;
+    }
+
+    void display() {
+        System.out.println("Name: " + name + ", RollNo: " + rollno + ", year: " + year + ", Major: " + major);
+    }
+}
+
+public class inheri {
+	 public static void main(String[] args) {
+	        Teacher t = new Teacher("Severus Snape", "Dark Arts", 1000000);
+	        CollegeStudent s = new CollegeStudent("Harry", 1, 7, "Magic");
+	        t.display();
+	        s.display();
+	    }
+}
